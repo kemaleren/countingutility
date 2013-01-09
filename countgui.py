55200fa0b8497160c47992f0a7ec1bc91f34abde
Kemal Eren <kemal.eren@iwr.uni-heidelberg.de>
2013-01-09 15:47:24 +0100

    updated

--- countgui.py
+++ countgui.py
@@ -5,16 +5,22 @@ import scipy.misc
 import numpy
 from PyQt4 import QtGui, QtCore
 
-SIZE = 5
+SIZE = 11
+
+class QDotSignaller(QtCore.QObject):
+    deletedSignal = QtCore.pyqtSignal(int, int)
+
+signaller = QDotSignaller()
 
 class QDot(QtGui.QGraphicsEllipseItem):
     _hoverColor    = QtGui.QColor(255, 0, 0, 120)
     _normalColor   = QtGui.QColor(0, 0, 255, 120)
-
+    
     def __init__(self, x, y, size):
         super(QDot, self).__init__(y - radius, x - radius, size, size)
         self._updateColor(self._normalColor)
-        self.setAcceptHoverEvents(True)
+#        self.setAcceptHoverEvents(True)
+        self.setAcceptedMouseButtons(QtCore.Qt.LeftButton)
         self.x = x
         self.y = y
         self.size = size
@@ -35,6 +41,11 @@ class QDot(QtGui.QGraphicsEllipseItem):
         radius = size / 2
         self.setRect(self.y - radius, self.x - radius, size, size)
         self.setCursor(QtCore.Qt.ArrowCursor)
+
+    def mousePressEvent(self, event):
+        event.setAccepted(True)
+        print 'deleting'
+        signaller.deletedSignal.emit(self.y, self.x)
         
     def _updateColor(self, color):
         self.setPen(QtGui.QPen(color))
@@ -53,9 +64,19 @@ if __name__ == "__main__":
     view = QtGui.QGraphicsView(scene)
     view.show()
 
+    pos_to_dot = {}
+
+    def del_dot(x, y):
+        dot = pos_to_dot.pop((x, y))
+        scene.removeItem(dot)
+    signaller.deletedSignal.connect(del_dot)
+
+  
     dots = scipy.misc.imread(sys.argv[2])
     for x, y in zip(*numpy.where(dots != 0)):
         radius = SIZE / 2
-        scene.addItem(QDot(x, y, SIZE))
+        dot = QDot(x, y, SIZE)
+        scene.addItem(dot)
+        pos_to_dot[(x, y)] = dot
 
     sys.exit(app.exec_())
